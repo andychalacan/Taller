8f941206b9b7353ff66131563b0456fa05ba02ad
andychalacan <168015224+andychalacan@users.noreply.github.com>
2024-10-04 12:59:14 -0500

    Add files via upload

---
+++ Proyecto/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Andy");
+        System.out.println("Apellido: Chalacan");
+        System.out.println("Telefono: 0963181026");
+
+        }
+    }
